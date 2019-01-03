eb5ab67f82fb4e6c8d573ac89a0de01b870d07e1
chenwei <chenwei@wenshengamc.com>
2019-01-03 17:12:49 +0800

    add for debt image

---
+++ zcc-amc/src/main/java/com/wensheng/zcc/amc/module/dao/mongo/entity/DebtImage.java
@@ -0,0 +1,22 @@
+package com.wensheng.zcc.amc.module.dao.mongo.entity;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "DEBT_IMAGE")
+@Data
+public class DebtImage {
+    String _id;
+    String description;
+    String path;
+    String originalName;
+    Boolean mainPic; //是否为主图片
+    int tag; //是否位置图， 其它图片
+    Boolean isToOss = false;//是否同步到OSS
+    Long originAssetId;
+    @Indexed(direction = IndexDirection.ASCENDING)
+    Long debtId;
+
+}

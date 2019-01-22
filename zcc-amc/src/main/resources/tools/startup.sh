4a2d9e633fe4fa73834816413e7366f72d570e6f
chenwei <chenwei@wenshengamc.com>
2019-01-22 17:59:03 +0800

    update script

--- zcc-amc/src/main/resources/tools/startup.sh
+++ zcc-amc/src/main/resources/tools/startup.sh
@@ -1,16 +1,16 @@
-!#/bin/bash
-export execJarName=/home/sunht/working/zcc/zcc-amc*.jar
+#!/bin/bash
+export execJarName=/home/sunht/working/zcc/zcc-amc-0.1-SNAPSHOT.jar
+export pattern=zcc
 killProcess() {
-    if [[ -z $1  ]]; then
-        echo "please input the process pattern to kill"
-    fi
-    result=`ps -ef | grep "$1" | grep -v grep | awk '{print $2}'|wc -l`
+    result=`ps -ef | grep "$pattern" | grep -v grep | awk '{print $2}'| wc -l`
     echo "now thre is $result process like:$1 are running"
     if [ $result -gt 0 ]; then
-        echo "to kill process with pattern:$1"
-        ps -ef | grep $1 | grep -v grep | awk '{print $2}' | xargs kill -9
+        echo "to kill process with pattern:$1 and pid:$result"
+        kill $(ps -ef | grep $pattern | grep -v grep | awk '{print $2}')
     fi
 }
 
 killProcess $1
-nohup java -jar -Dspring.profiles.active=dev $execJarName &>zcc.log &
+echo ${execJarName}
+sleep 5
+nohup java -jar -Dspring.profiles.active=dev ${execJarName} &>zcc.log &

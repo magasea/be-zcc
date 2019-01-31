c091e8597d2a3cb22211427b0599a5f2cea8183a
chenwei <chenwei@wenshengamc.com>
2019-01-31 16:00:34 +0800

    add test files

---
+++ zcc-sso/src/main/java/com/wensheng/zcc/sso/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.wensheng.zcc.sso.controller;
+
+import org.hibernate.validator.constraints.pl.REGON.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author chenwei on 1/31/19
+ * @project zcc-backend
+ */
+@Controller
+
+public class UserControll {
+
+  private static final String VERIFY_CODE = "verify_code";
+  private static final String VERIFY_CODE_IMAGE = "verify_code_image";
+
+
+  @RequestMapping(value = "/user/verifycode")
+  public boolean verifyVCode(String vcode){
+
+  }
+
+  @RequestMapping(value = "user/getvc")
+  public
+
+}

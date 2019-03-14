079d249209acd86ff74cdce35bd98676765296dc
chenwei <chenwei@wenshengamc.com>
2019-03-14 18:49:52 +0800

    update for pom

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mvn clean -DskipTests package
+mvn clean compile -DskipTests package
 scp zcc-sso/target/zcc-sso-0.1-SNAPSHOT.jar sunht@10.20.100.236:/home/sunht/working/zcc
 scp zcc-amc/target/zcc-amc-0.1-SNAPSHOT.jar sunht@10.20.100.236:/home/sunht/working/zcc

72c30af52dddbca8a26d8b1e365bdecf80df4d42
chenwei <chenwei@wenshengamc.com>
2019-03-20 14:10:51 +0800

    update appid

--- zcc-sso/src/main/java/com/wensheng/zcc/sso/service/impl/WechatServiceImpl.java
+++ zcc-sso/src/main/java/com/wensheng/zcc/sso/service/impl/WechatServiceImpl.java
@@ -71,7 +71,7 @@ public class WechatServiceImpl implements WechatService {
 
     }
 
-    amcWechatUserExample.createCriteria().andSessionKeyEqualTo()
+//    amcWechatUserExample.createCriteria().andSessionKeyEqualTo()
 
     return null;
   }

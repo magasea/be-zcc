0e09229ebfa7a7c398fdf912f7bc7ca9dc2e6f40
chenwei <chenwei@wenshengamc.com>
2019-04-01 10:46:34 +0800

    remove unnecessary files

---
+++ env-tool/kafka.sh
@@ -0,0 +1,2 @@
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic zcc-amc-useroperation
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic zcc-wechat-useroperation

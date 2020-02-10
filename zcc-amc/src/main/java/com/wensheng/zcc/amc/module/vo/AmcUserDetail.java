d21e5856675c2fc27df014e7537b6627fa01a2f4
chenwei <chenwei@wenshengamc.com>
2020-02-10 15:02:11 +0800

    add serialable

--- zcc-amc/src/main/java/com/wensheng/zcc/amc/module/vo/AmcUserDetail.java
+++ zcc-amc/src/main/java/com/wensheng/zcc/amc/module/vo/AmcUserDetail.java
@@ -1,5 +1,6 @@
 package com.wensheng.zcc.amc.module.vo;
 
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.Date;
 import lombok.Data;
@@ -12,7 +13,7 @@ import org.springframework.security.core.userdetails.User;
  * @project miniapp-backend
  */
 @Data
-public class AmcUserDetail extends User {
+public class AmcUserDetail extends User implements Serializable {
 
   private Long id;
 

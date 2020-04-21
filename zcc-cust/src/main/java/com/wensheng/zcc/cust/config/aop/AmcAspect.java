c4636389233518d38b58578e89bfda97512df526
chenwei <chenwei@wenshengamc.com>
2020-04-21 17:31:06 +0800

    update

--- zcc-cust/src/main/java/com/wensheng/zcc/cust/config/aop/AmcAspect.java
+++ zcc-cust/src/main/java/com/wensheng/zcc/cust/config/aop/AmcAspect.java
@@ -129,7 +129,7 @@ public class AmcAspect {
   @Around("@annotation(QueryCheckerCmpy) ")
   public Object aroundDoModCmpy(ProceedingJoinPoint joinPoint) throws Throwable {
     log.info("now get the point cut");
-    if(joinPoint.getArgs().length < 1 || joinPoint.getArgs()[0] == null){
+    if (joinPoint.getArgs().length < 1 || joinPoint.getArgs()[0] == null) {
       log.error("cannot process check for this method with args:{}", joinPoint.getArgs());
       return joinPoint.proceed(joinPoint.getArgs());
     }
@@ -138,64 +138,64 @@ public class AmcAspect {
     Map<String, Integer> userPrivMap = basicInfoService.getAmcUserPrivMap();
 
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    if(authentication == null || ! (authentication.getDetails() instanceof OAuth2AuthenticationDetails)){
+    if (authentication == null || !(authentication.getDetails() instanceof OAuth2AuthenticationDetails)) {
       throw ExceptionUtils.getAmcException(AmcExceptions.LOGIN_REQUIRE_ERROR);
     }
     log.info(authentication.getDetails().toString());
 
 
     Map<String, Object> detailsParam =
-        (Map<String, Object>) ((OAuth2AuthenticationDetails)authentication.getDetails()).getDecodedDetails();
-    if(detailsParam.containsKey("ssoUserId") && null != detailsParam.get("ssoUserId")){
-      Long ssoUserId = Long.valueOf((Integer)detailsParam.get("ssoUserId"));
-      if(joinPoint.getSignature().getName().startsWith("add") || joinPoint.getSignature().getName().startsWith(
-          "create")){
+            (Map<String, Object>) ((OAuth2AuthenticationDetails) authentication.getDetails()).getDecodedDetails();
+    if (detailsParam.containsKey("ssoUserId") && null != detailsParam.get("ssoUserId")) {
+      Long ssoUserId = Long.valueOf((Integer) detailsParam.get("ssoUserId"));
+      if (joinPoint.getSignature().getName().startsWith("add") || joinPoint.getSignature().getName().startsWith(
+              "create")) {
         custTrdCmpy.setCreateBy(ssoUserId);
         custTrdCmpy.setCreateTime(AmcDateUtils.getCurrentDate());
       }
-      if(joinPoint.getSignature().toString().startsWith("update") || joinPoint.getSignature().toString().startsWith(
-          "mod")){
+      if (joinPoint.getSignature().toString().startsWith("update") || joinPoint.getSignature().toString().startsWith(
+              "mod")) {
         custTrdCmpy.setUpdateBy(ssoUserId);
         custTrdCmpy.setUpdateTime(AmcDateUtils.getCurrentDate());
       }
 
     }
 
-    if(CollectionUtils.isEmpty(userPrivMap) || StringUtils.isEmpty(province) ){
+    if (CollectionUtils.isEmpty(userPrivMap) || StringUtils.isEmpty(province)) {
 
       return joinPoint.proceed(joinPoint.getArgs());
     }
 
-    if(detailsParam.containsKey("location") && null != detailsParam.get("location")){
+    if (detailsParam.containsKey("location") && null != detailsParam.get("location")) {
       Integer locationId = (Integer) detailsParam.get("location");
 
       AmcLocationEnum locationUserEnum =
-          AmcLocationEnum.lookupByDisplayIdUtil(locationId) ;
+              AmcLocationEnum.lookupByDisplayIdUtil(locationId);
       AmcLocationEnum designedLocationEnum = AmcLocationEnum.lookupByDisplayIdUtil(userPrivMap.get(province));
 
-      if(!userPrivMap.get(province).equals(locationId)){
+      if (!userPrivMap.get(province).equals(locationId)) {
 
-      log.info("===============================");
-      log.info("userPrivMap===>{}",userPrivMap);
-      log.info("province===>{}",province);
-      log.info("locationId==>",locationId);
+        log.info("===============================");
+        log.info("userPrivMap===>{}", userPrivMap);
+        log.info("province===>{}", province);
+        log.info("locationId==>", locationId);
 //      if(locationId == null || locationId.compareTo(0) < 0 || designedLocationEnum ==null || locationUserEnum == null){
 //        throw new RuntimeException(String.format("没有归属地区的用户不能更改投资入库"));
 //      }
 
-      if(!userPrivMap.get(province).equals(locationId)){
-        throw new RuntimeException(String.format("您所在的地区:%s 不能处理该省的投资人信息, 按照设计应该由:%s 地区的业务人员来处理",
-            locationUserEnum.getCname(), designedLocationEnum.getCname()));
+        if (!userPrivMap.get(province).equals(locationId)) {
+          throw new RuntimeException(String.format("您所在的地区:%s 不能处理该省的投资人信息, 按照设计应该由:%s 地区的业务人员来处理",
+                  locationUserEnum.getCname(), designedLocationEnum.getCname()));
+        }
       }
+
     }
-
-
-    return joinPoint.proceed(joinPoint.getArgs());
+      return joinPoint.proceed(joinPoint.getArgs());
   }
 
 
 
-  @Around("@annotation(QueryValidCmpy) ")
+  @Around("@annotation(QueryValidCmpy)")
   public Object aroundQueryValidCmpy(ProceedingJoinPoint joinPoint) throws Throwable {
     log.info("now get the point cut");
     if(joinPoint.getArgs().length < 1 || joinPoint.getArgs()[0] == null){

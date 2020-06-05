94346ec408075a8721d1b032fa948a78f332e5c8
wangshouzheng <1635057818@qq.com>
2020-06-05 17:25:42 +0800

    tj

--- zcc-cust/src/main/java/com/wensheng/zcc/cust/controller/CustPersonConytoller.java
+++ zcc-cust/src/main/java/com/wensheng/zcc/cust/controller/CustPersonConytoller.java
@@ -4,7 +4,10 @@ import com.wensheng.zcc.cust.config.aop.AddTraceLogId;
 import com.wensheng.zcc.cust.config.aop.ModifyCheckerCustCmpycontactor;
 import com.wensheng.zcc.cust.module.dao.mysql.auto.entity.CustAmcCmpycontactor;
 import com.wensheng.zcc.cust.module.dao.mysql.auto.entity.CustTrdPerson;
+import com.wensheng.zcc.cust.service.CustPersonService;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,14 +20,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/amc/cust/custPersonConytoller")
 public class CustPersonConytoller {
 
+  @Autowired
+  CustPersonService custPersonService;
+
   @RequestMapping(value = "/addCmpyAmcContactor", method = RequestMethod.POST)
   @ResponseBody
-  @ModifyCheckerCustCmpycontactor
-  public void addCmpyAmcContactor(@RequestBody CustTrdPerson custTrdPerson) throws Exception {
-
-
-
-
+  public void addCustPerson(@RequestBody CustTrdPerson custTrdPerson) throws Exception {
+    custPersonService.createCustPerson(custTrdPerson);
   }
 
+  @RequestMapping(value = "/updateCustPerson", method = RequestMethod.POST)
+  @ResponseBody
+  public void updateCustPerson(@RequestBody CustTrdPerson custTrdPerson) throws Exception {
+    custPersonService.updateCustPerson(custTrdPerson);
+  }
+
+  @RequestMapping(value = "/mergeCustPerson", method = RequestMethod.POST)
+  @ResponseBody
+  public void mergeCustPerson(@RequestBody List<Long> fromPersonIds,@RequestBody Long toPersonId) throws Exception {
+    custPersonService.mergeCustPerson(fromPersonIds, toPersonId);
+  }
 }

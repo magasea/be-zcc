f5410932188c0de74f7a369eb35390cf0476a529
wangshouzheng <1635057818@qq.com>
2020-07-14 18:31:58 +0800

    tj

--- zcc-cust/src/main/java/com/wensheng/zcc/cust/service/impl/CustInfoServiceImpl.java
+++ zcc-cust/src/main/java/com/wensheng/zcc/cust/service/impl/CustInfoServiceImpl.java
@@ -927,10 +927,12 @@ public class CustInfoServiceImpl implements CustInfoService {
    */
   private String creatPhonePrep (String cmpyPhone, String annuReptPhone){
     StringBuffer sbPhoneNew = new StringBuffer();
-    if((!"-1".equals(cmpyPhone) && !"暂无信息".equals(cmpyPhone)) && !StringUtils.isEmpty(cmpyPhone)){
+    if((!"-1".equals(cmpyPhone) && !"暂无信息".equals(cmpyPhone)) && !StringUtils.isEmpty(cmpyPhone)
+    && !"-".equals(cmpyPhone)){
       sbPhoneNew.append(cmpyPhone);
     }
-    if((!"-1".equals(annuReptPhone) && !"暂无信息".equals(annuReptPhone)) && !StringUtils.isEmpty(annuReptPhone)){
+    if((!"-1".equals(annuReptPhone) && !"暂无信息".equals(annuReptPhone)) && !StringUtils.isEmpty(annuReptPhone)
+        && !"-".equals(cmpyPhone)){
       if(!annuReptPhone.equals(cmpyPhone)){
         if(sbPhoneNew.length()>0){
           sbPhoneNew.append(";");
